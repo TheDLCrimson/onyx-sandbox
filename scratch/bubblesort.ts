40b85b1bc37ee8a5862d75a07dcd86a1b4e58afa
TheDLCrimson <52397226+TheDLCrimson@users.noreply.github.com>
2026-04-27 13:39:22 +0700

    Create scratch/bubblesort.ts via Discord buddy

---
+++ scratch/bubblesort.ts
@@ -0,0 +1,19 @@
+scratch/bubblesort.ts
+function bubbleSort(arr: number[]): number[] {
+  const result = [...arr];
+  const n = result.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (result[j] > result[j + 1]) {
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  return result;
+}
+
+export { bubbleSort };
